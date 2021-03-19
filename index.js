a5810095d2fb18889fe826b6e867be0feafe6a2e
Francisco <fnaveira11@gmail.com>
2021-03-19 14:08:54 -0400

    commit tree 408b7cd2e7209769f810d6febe0b0148933d9ea0

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+  
+function superbowlWin(record) {
+    const result = record.find(function (element) {
+        return element.result === 'W'
+      }
+      )  
+      if (result){ return result.year}
+      else { return undefined}
+}
+
+
+  
+  
+  
